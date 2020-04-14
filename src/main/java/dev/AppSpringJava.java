167d9d70706a82184425b4f4707d5d4dc18fc20a
Jonathan Sagan <sagan.jonathan@gmail.com>
2020-04-14 10:18:56 +0200

    TP4.1 ok

---
+++ src/main/java/dev/AppSpringJava.java
@@ -0,0 +1,26 @@
+package dev;
+
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import dev.config.AppConfig;
+import dev.ihm.Menu;
+
+/**
+ * @author Jonathan Sagan
+ *
+ */
+public class AppSpringJava {
+
+	public static void main(String[] args) {
+		// Création du contexte Spring à partir d'une configuration Java
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(AppConfig.class);
+
+		// récupération du bean Menu
+		Menu menu = context.getBean(Menu.class);
+		menu.afficher();
+
+		// fermeture du contexte Spring
+		context.close();
+	}
+
+}
